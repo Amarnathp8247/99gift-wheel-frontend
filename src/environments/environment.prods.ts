b1176d67333e23e3969befe2417480bfb55db7d0
Amarnath <amarnath@99gift.in>
2025-07-21 10:28:03 +0530

    new update

--- src/environments/environment.prods.ts
+++ src/environments/environment.prods.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//     production: true,
-//     apiBaseUrl: 'https://devapi.gamerpitch.in/app/v1'
-//   };
-
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://foodiqr.xyz/api/v1'
+  apiBaseUrl: 'http://localhost:8000/api/v1'
 };
+
+// export const environment = {
+//   production: true,
+//   apiBaseUrl: 'https://foodiqr.xyz/api/v1'
+// };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,11 @@
+
 // export const environment = {
 //     production: false,
-//     apiBaseUrl: 'http://localhost:8000/api/v1'
+//    apiBaseUrl: 'https://foodiqr.xyz/api/v1'
 //   };
-export const environment = {
-    production: false,
-   apiBaseUrl: 'https://foodiqr.xyz/api/v1'
+ 
+ 
+  export const environment = {
+    production: true,
+    apiBaseUrl: 'http://localhost:8000/api/v1'
   };
- 
- 
-  // export const environment = {
-  //   production: true,
-  //   apiBaseUrl: 'https://devapi.gamerpitch.in/api/v1'
-  // };
